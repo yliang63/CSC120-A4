5e144bf9bd3132f2f16cb4d1ed4d6c99270fe3c3
yliang <yliang6@smith.edu>
2024-09-30 04:05:21 +0800

    engine class

--- Engine.java
+++ Engine.java
@@ -1,3 +1,40 @@
-public class Engine {
 
+
+public class Engine {
+  private FuelType fuelType;
+  private double currentLevel;
+  private double MaxLevel;
+
+  public enum FuelType {
+    ELECTRIC,
+    GASOLINE,
+    DIESEL
+  }
+  public Engine(FuelType fueltype,double max){
+    this.fuelType= fueltype;
+    this.currentLevel=max;
+    this.MaxLevel=max;
+  }
+
+  public void refuel(){
+    currentLevel=MaxLevel;
+  }
+  public boolean go(){
+    if(currentLevel>0){
+        System.out.println(currentLevel);
+        currentLevel--;
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+  }
+  public static void main(String[] args) {
+    Engine myEngine = new Engine(FuelType.ELECTRIC,100.0);
+    while (myEngine.go()) {
+        System.out.println("Choo choo!");
+    }
+    System.out.println("Out of fuel.");
+}
 }
