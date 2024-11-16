ecccd53bdbbf36d9d1a3b04b3c3307fef174e0b9
yliang <yliang6@smith.edu>
2024-11-16 14:26:33 -0500

    train javadoc

--- Train.java
+++ Train.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
 
+/**
+ * The Train class demonstrates a train consisting of an engine and multiple cars.
+ */
 public class Train {
     private Engine engine;
     private ArrayList<Car> cars;
     
-    //constructor
+    /**
+     * Constructs a Train object with the specified engine and a number of cars.
+     *
+     * @param fuelType The type of fuel the engine uses.
+     * @param fuelCapacity The fuel capacity of the engine.
+     * @param numbersOfCars The number of cars in the train.
+     * @param passengerCapacity The passenger capacity of each car.
+     */
     public Train(FuelType fuelType, double fuelCapacity, int numbersOfCars, int passengerCapacity) {
         engine = new Engine(fuelType, fuelCapacity);
         cars = new ArrayList<>();
@@ -14,12 +24,21 @@ public class Train {
         }
     }
 
-    //get method from engine class
+    /**
+     * Returns the engine of the train.
+     * 
+     * @return The engine of the train.
+     */
     public Engine getEngine() {
         return engine;
     }
 
-    //get method from car class
+    /**
+     * Returns a specific car from the train based on its index.
+     * 
+     * @param i The index of the car to return.
+     * @return The car at a specified index, or null if the index is out of the range.
+     */
     public Car getCar(int i) {
         if (i>=0 && i<cars.size()) {
             return cars.get(i);
@@ -27,7 +46,11 @@ public class Train {
         return null;
     }
     
-    //get maxcapacity of car,encapsulation
+    /**
+     * Calculates the maximum passenger capacity of the train.
+     * 
+     * @return The number of maximum passengers capacity of the train.
+     */
     public int getMaxCapacity() {
         int maxCapacity = 0;
         for (Car car : cars) {
@@ -36,7 +59,11 @@ public class Train {
         return maxCapacity;
     }
 
-    //get seats availalbe,encapsulation
+    /**
+     * Calculates the number of remaining seats in the train.
+     * 
+     * @return The number of available seats.
+     */
     public int seatsRemaining() {
         int OccupiedSeats = 0;
         for (Car car : cars) {
@@ -45,13 +72,20 @@ public class Train {
         return getMaxCapacity()-OccupiedSeats;
     }
 
-    //encapsulation
+    /**
+     * @Encapsulation
+     * Prints the manifest of the train, including details of all cars.
+     */
     public void printManifest() {
         System.out.println("Train Manifest:");
         for (Car car : cars) {
             car.printManifest();
         }
     }
+
+    /**
+     * This main method is to demonstrate the functionality of the train.
+     */
     public static void main(String[] args) {
         Train train = new Train(FuelType.STEAM, 100.0, 3, 50);
         System.out.println("Train Engine Details:");
